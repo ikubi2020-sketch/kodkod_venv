05d8c42fd0623434566ff5de07e3fbb1ebdf9588
yehonatan koobi <ikubi2020@gmail.com>
2026-06-03 17:07:22 +0300

    added file handler

---
+++ project_api_1/logger_config.py
@@ -0,0 +1,15 @@
+import logging
+
+logging.basicConfig(
+    level= logging.INFO,
+    format="%(asctime)s, %(levelname)s, %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+file_handler =  logging.FileHandler("system.log", encoding="utf_8")
+
+formatter = logging.Formatter("%(asctime)s, %(levelname)s, %(message)s")
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
